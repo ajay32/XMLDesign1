3a161669bbd8c1cda332e86ae495cdefb3ab94bd
Ajay Mehta <dev.ajaymehta@gmail.com>
2017-08-04 11:22:25 -0700

    added things to remember

--- app/src/main/java/ajaymehta/xmldesign1/MainActivity.java
+++ app/src/main/java/ajaymehta/xmldesign1/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // things to remember
+        //1.  giving padding to top parent ..squeeze all the child elements..
+        //2.  giving margin to particular child ..squeeze that particular child..
+        // 3. giving padding to particular child  ..makes it big ..
     }
 }
